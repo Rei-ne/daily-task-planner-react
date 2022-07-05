78994e76e81a1fcced6063b5600734947f0e75cc
Toyosi <reinetoyosii@gmail.com>
2022-07-05 11:33:44 +0100

    mobile responsiveness for smaller screens

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ import { useState, useEffect } from 'react'
 
 
 function App() {
-  // Use state to add tasks
+  // Use state to add tasks w button
   const [showAddTasks, setShowAddTasks] = useState(false)
 
   const [tasks, setTasks] = useState([])
 
-  // getting tasks from the json db
+  // getting tasks from the localhost json db
   useEffect(() => {
 
     const getTasks = async () => {
@@ -42,21 +42,12 @@ function App() {
 
   }
 
-  // fetch task
-
-  // const fetchTask = async (id) => {
-
-  //   const res = await fetch(`http://localhost:5000/tasks/${id}`)
-  //   const data = await res.json()
-
-  //   return data
-
-  // }
 
 
 
 
-  //Add task
+
+  //promise to add task
   const addTask = async (task) => {
     const res = await fetch('http://localhost:5000/tasks', {
       method: 'POST',
@@ -77,12 +68,13 @@ function App() {
   }
 
 
-  // Delete tasks
+  // promise to delete tasks
   const deleteTask = async (id) => {
     const res = await fetch(`http://localhost:5000/tasks/${id}`, {
       method: 'DELETE',
 
     })
+    // error checking
     res.status === 200
       ? setTasks(tasks.filter((task) => task.id !== id))
       : alert('Error Deleting This Task')
@@ -90,38 +82,11 @@ function App() {
 
   }
 
-
-  // Toggle reminder
-  // const toggleReminder = async (id) => {
-  //   const taskToToggle = await fetchTask(id)
-  //   const updateTask = {
-  //     ...taskToToggle, reminder: !taskToToggle.reminder
-  //   }
-
-  //   const res = await fetch(`http://localhost:5000/tasks/${id}`,
-  //     {
-  //       method: 'PUT',
-  //       headers: {
-  //         'Content-type': 'application/json',
-
-  //       },
-  //       body: JSON.stringify(updateTask)
-
-  //     }
-  //   )
-
-  //   const data = await res.json()
-
-  //   setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: data.reminder } : task)
-  //   )
-  // }
-
-
   return (
     <div className="container">
       <Header onAdd={() => setShowAddTasks(!showAddTasks)} showAdd={showAddTasks} />
       {showAddTasks && <AddTask onAdd={addTask} />}
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} /> : 'You have no tasks'}
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} /> : <h4 className="paragraph">You have no tasks</h4>}
     </div>
     // onToggle={toggleReminder}
   );
--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 const Button = ({ color, onClick, text }) => {
 
     return (
-        <button onClick={onClick} style={{ backgroundColor: color }} className="btn"> {text}</button>
+        <button onClick={onClick} style={{ backgroundColor: color }} className="btn showbtn"> {text}</button>
     )
 }
 
